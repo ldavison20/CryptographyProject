dca77e5a3218fb5ce67a9823fdd60b65855a10f3
ldavison20 <43002623+ldavison20@users.noreply.github.com>
2019-04-11 23:02:49 -0400

    Add files via upload

--- src/Cipher.java
+++ src/Cipher.java
@@ -3,17 +3,41 @@ public class Cipher
 {
 	public static void main(String[] args)
 	{
-	Scanner keyboard = new Scanner(System.in);
-	System.out.print("Please enter a file to encrypt: ");
-	String file = keyboard.nextLine();
-	for(int i = 0; i < file.length(); i++)
-		{
-			char  letter = file.charAt(i);
-			int num = letter;
-			num = (num - -4) % 26;
-			System.out.print((char)(num + 'a'));
-		}
-	
+		Scanner keyboard = new Scanner(System.in);
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("Enter name of file: ");
+        String file = keyboard.nextLine();
+        for(int i = 0; i < file.length(); i++)
+        {
+            char letter = file.charAt(i);
+            int index = alphabet.indexOf(letter);
+            if(!isLower(letter) && !isUpper(letter))
+            {
+                System.out.print(letter);
+            }
+            
+            else
+            {
+                if(isLower(letter))
+                {
+                	System.out.print(alphabet.charAt(26 - Math.abs(index - 3)));
+                }
+                
+                else if(isUpper(letter))
+                {
+                    System.out.print(alphabetUpper.charAt((3 % 26) + 26) % 26);
+                }
+            }
+        }
 	}
-
+	public static boolean isLower(char c)
+	{
+		return c >= 'a' && c <= 'z';
+	}
+	public static boolean isUpper(char c)
+	{
+		return c >= 'A' && c >= 'C';
+	}
 }
+
